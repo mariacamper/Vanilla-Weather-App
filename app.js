8280b13ba0f45b759b643fbec19136eb0e63e3c6
mariacamper <m.campsite@yahoo.com>
2023-07-03 00:28:10 -0400

    Update time

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
+
+function formatDate(timestamp) {
+  //calculate the date
+  let date = new Date(timestamp);
+
+  let hours = date.getHours();
+   if (hours < 10) {
+   hours = `0${hours}`;
+ }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+  
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+
+  
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-  console.log(response.data);
+
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 
@@ -14,6 +36,9 @@ function displayTemperature(response) {
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 
