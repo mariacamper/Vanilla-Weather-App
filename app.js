abe5be2f80ec062ec05375131856c6a4da171421
mariacamper <m.campsite@yahoo.com>
2023-07-04 09:51:23 -0400

    Added forecast to js injecting html

--- app.js
+++ app.js
@@ -20,6 +20,31 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday"];
+  days.forEach(function (day) {
+    
+     forecastHTML = forecastHTML + `
+    
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img src="sun.png" alt="" width="42"/>
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">18&deg</span>
+            <span class="weather-forecast-temperature-min">12&deg</span>
+          </div>
+      </div>`;
+   
+
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayTemperature(response) {
   
   let temperatureElement = document.querySelector("#temperature");
@@ -89,3 +114,4 @@ let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemperature);
 
 search("New York");
+displayForecast();
